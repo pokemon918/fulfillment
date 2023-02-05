3e1dcc7fec534e3fae938aea1338d1897a5ecd91
pokemon918 <j27394645@gmail.com>
2023-02-05 15:06:59 +0200

    fix: improve performance by removing app's getInitialProps

--- src/hooks/useFetchAuthUser.ts
+++ src/hooks/useFetchAuthUser.ts
@@ -14,57 +14,14 @@ const GET_USER_ME = gql`
   }
 `
 
-const useFetchAuthUser = (storedUserString: string | null) => {
-  const [user, setUser] = useState<AuthUser | null>(
-    (() => {
-      if (storedUserString) {
-        try {
-          const parsed = JSON.parse(decodeURIComponent(storedUserString))
-
-          if (
-            parsed &&
-            typeof parsed === 'object' &&
-            typeof parsed._id === 'string' &&
-            typeof parsed.fullName === 'string' &&
-            typeof parsed.role === 'string'
-          ) {
-            return parsed
-          }
-        } catch {}
-      }
-
-      return null
-    })()
-  )
+const useFetchAuthUser = () => {
+  const [user, setUser] = useState<AuthUser | null | undefined>()
 
   useEffect(() => {
     ;(async () => {
       const { fetchedUser } = await graphqlReq(GET_USER_ME)
 
-      if (fetchedUser) {
-        if (
-          !user ||
-          user._id !== fetchedUser._id ||
-          user.fullName !== fetchedUser.fullName ||
-          user.role !== fetchedUser.role
-        ) {
-          const YEAR = 1000 * 60 * 60 * 24 * 365
-          const expireAt = new Date(Date.now() + YEAR)
-          
-          setCookie(
-            'token_user',
-            encodeURIComponent(JSON.stringify(fetchedUser)),
-            expireAt
-          )
-
-          setUser(fetchedUser)
-        }
-      } else {
-        deleteCookie('token')
-        deleteCookie('token_user')
-
-        if (user) setUser(null)
-      }
+      setUser(fetchedUser)
     })()
   }, [])
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import GlobalStyles from '@/GlobalStyles'
-import type { AppContext, AppProps } from 'next/app'
-import NextApp from 'next/app'
+import type { AppProps } from 'next/app'
 import 'flag-icons/css/flag-icons.min.css'
 import smoothScroll from 'smoothscroll-polyfill'
 import NavigatingIndicator from '@/components/NavigatingIndicator'
 import { UserProvider } from '@/contexts/userContext'
 import useFetchAuthUser from '@/hooks/useFetchAuthUser'
-import { getCookie } from '@/utils/cookies'
 import Head from 'next/head'
-import NoSSR from '@/components/NoSSR'
 
 if (typeof window !== 'undefined') {
   smoothScroll.polyfill()
@@ -17,14 +14,17 @@ if (typeof window !== 'undefined') {
 export default function App({
   Component,
   pageProps,
-  storedUserString,
 }: AppProps & { storedUserString: string | null }) {
-  const user = useFetchAuthUser(storedUserString)
+  const user = useFetchAuthUser()
+
+  if (typeof user === 'undefined') return null
 
   return (
     <>
       <Head>
-        <title>TRU MARKET - Trust, transparency & traceability in agro market</title>
+        <title>
+          TRU MARKET - Trust, transparency & traceability in agro market
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -37,15 +37,4 @@ export default function App({
       </UserProvider>
     </>
   )
-}
-
-App.getInitialProps = async (context: AppContext) => {
-  const defaultInitialProps = await NextApp.getInitialProps(context)
-
-  const cookies = context.ctx.req?.headers.cookie ?? ''
-
-  return {
-    ...defaultInitialProps,
-    storedUserString: getCookie(cookies, 'token_user'),
-  }
 }
