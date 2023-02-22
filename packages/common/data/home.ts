913e424cb2d3f9964e343e4c2a8b14c24153d3fc
pokemon918 <j27394645@gmail.com>
2023-02-22 00:47:12 -0500

    Update home.ts

--- packages/common/data/home.ts
+++ packages/common/data/home.ts
@@ -1,12 +1,12 @@
 export const homeData = {
   banner: {
-    title: 'Smart Contracts for global trade and sourcing',
-    content: 'Experience seamless global trade with our smart contracts solution. Our technology ensures secure, transparent transactions while we focus on traceability and financing of operations to deliver the best quality products.'
+    title: 'Smart Contracts for global trade',
+    content: 'Experience seamless global trade with our smart contracts solution. \nOur technology ensures secure, transparent transactions while we focus on traceability and financing of operations to deliver the best quality products.'
   },
   solutionsOverview: {
-    heading: 'Overview of TRU MARKET smart contracts', 
-    title: 'TRU MARKET',
-    content: 'Our platform TRU MARKET offers a secure, transparent, and efficient way to conduct global trade. Buyers and Suppliers can create smart contracts that specify the conditions for their operations, including milestones in the value chain from harvest to delivery. Parties can review and agree to these conditions, knowing that payments will be made only when the milestones set are reached. TRU Market inspects every operation that takes place on the platform to ensure that the milestones are being fulfilled accordingly, providing real-time traceability and transparency for all parties involved. Lenders can fund these operations, knowing that the smart contract and milestone-based payment structure provides a high degree of security and transparency.'    
+    heading: 'How TRU MARKET is shaping the way to conduct international business', 
+    title: 'TRU MARKET Smart Contracts',
+    content: 'TRU MARKET offers a secure, transparent, and efficient way to conduct global trade. Buyers and Suppliers can create smart contracts that specify the agreed conditions of the business, including milestones in the value chain from start to delivery. Parties can review and agree to these conditions, knowing that payments will be made only when the milestones set are reached. \nTRU Market inspects every operation that takes place on the platform to ensure that the milestones are being fulfilled accordingly, providing real-time traceability and transparency for all parties involved. \nLenders can fund these operations, while the smart contracts and milestone-based payment structure provides a high degree of security and transparency.'    
   },
   blogOverview: 'Find updated market intelligence and volume-price trends.',
   footerText: 'Our mission is to help suppliers connect with global buyers and grow their operations, through a smart contracts based platform that ensures transparency and trust on the value chain from farm to table, and financing of their operations'
