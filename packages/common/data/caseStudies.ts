2354278664a83fa0d24332a45daa708cde0c7722
pokemon918 <j27394645@gmail.com>
2023-04-19 23:03:45 +0000

    main

--- packages/common/data/caseStudies.ts
+++ packages/common/data/caseStudies.ts
@@ -14,7 +14,13 @@ import mangoe1 from '../assets/images/20230111_163645.jpg'
 import mangoe2 from '../assets/images/20230111_193734.jpg'
 import globalgap from '../assets/images/global_gap.jpg'
 import euorganic from '../assets/images/eu_organic.png'
-
+import gingero1 from '../assets/images/RECEPCION DE MATERIA PRIMA.jpg'
+import gingerp1 from '../assets/images/EMPAQUE.jpg'
+import gingerp2 from '../assets/images/EMPAQUE (1).jpg'
+import gingere1 from '../assets/images/photo_5019684066345200851_y.jpg'
+import gingere2 from '../assets/images/photo_5019684066345200815_y.jpg'
+import banner_ginger from '../assets/images/ginger_banner.jpg'
+import profile_ginger from '../assets/images/gingerprofile.jpg'
 
 export interface CaseStudy {
   slug: string
@@ -95,10 +101,10 @@ export const caseStudies: CaseStudy[] = [
     profile: {
       name: 'RTE Amazon',
       location: 'Junin, Peru',
-      cover: cover.src,
-      image: apromalpi.src,
+      cover: banner_ginger.src,
+      image: profile_ginger.src,
       type: {
-        title: 'Grower / Processor',
+        title: 'Processor / Exporter',
         icon: typeImg.src,
       },
     },
@@ -107,32 +113,37 @@ export const caseStudies: CaseStudy[] = [
     overviewImg: overviewImg.src,
     certifications: [certPlaceholder.src, certPlaceholder.src],
     journey: [
-      {
-        title: 'Plantation',
+        {
+        title: 'Harvest - milestone 1',
         description:
-          `The production of organic fresh mango begins in carefully nurtured orchards located in the northern regions of Peru - at Tambogrande - in orchads where the mango trees are cultivated with utmost care and precision. From planting to harvesting, every step is crucial in ensuring the highest quality mangoes reach the consumers' table. TRU MARKET's innovative platform has enabled traceability in the production orchards, bringing transparency and trust to the entire journey.
-
-Through smart contracts, TRU MARKET ensures that suppliers comply with all the milestones of the operation. This includes using certified organic farming practices, adhering to Codex Alimentarius standards for food safety, and obtaining relevant certifications such as GlobalGAP for good agricultural practices. TRU MARKET's platform records and verifies each operation in real-time, creating an immutable and transparent audit trail.
-
-With TRU MARKET's technology, buyers can confidently trace the origin of the mangoes, ensuring that they are sourced from reputable organic farms. Our platform enables recording and verification of planting details, including the variety of mangoes, planting date, and location. This information is then securely stored on the blockchain, providing an immutable record of the mangoes' origin. This enhances trust between buyers and suppliers, as the platform ensures that all operations are thoroughly inspected to meet the highest standards.`,
-        gallery: [mangoo1.src, ],
+          'Pichanaki, located in the heart of Peru, is known for its lush landscapes and favorable climate, making it the perfect region for cultivating organic fresh ginger. The fertile soil, ample rainfall, and abundant sunshine create optimal conditions for growing ginger with its unique flavors and characteristics.'+
+          'At our production orchards in Pichanaki, we take pride in employing sustainable farming practices through growers that follow organic farming methods and comply with all the certifications.'+
+          'Our platform plays a crucial role in capturing and recording each step of the cultivation process in real-time, ensuring transparency and authenticity. This allows us to ensure that our ginger is sourced from reputable farmers who follow sustainable and organic practices, and it provides transparency for our customers and end consumers. With our platform, we can confidently showcase the unique characteristics of the ginger produced in Pichanaki, highlighting its superior taste, aroma, and nutritional properties, all while maintaining the highest standards of sustainability and authenticity.',
+        gallery: [gingero1.src, ],
       },
       {
-        title: 'Plantation',
+        title: 'Packing - milestone 2',
         description:
-          'Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size. Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.',
-        gallery: [sampleImg.src, sampleImg.src],
+          'At our packing house, we uphold the same commitment to quality and traceability as we do in the production orchards. Our platform facilitates traceability in the packing house, where the organic fresh ginger undergoes meticulous sorting, cleaning, and packaging procedures.'+
+          'The ginger roots are carefully sorted to ensure that only the highest quality ginger makes it to the next stage of the process. Our experienced workers meticulously clean the ginger, removing any dirt or impurities, and ensuring that it meets our strict quality standards.'+
+          'Once cleaned, the ginger is carefully packaged using precision equipment to maintain its freshness and flavor. Our packaging materials are chosen to be environmentally friendly, ensuring that our commitment to sustainability is upheld throughout the entire process. Each package is carefully labeled with relevant information, including the origin of the ginger, its organic certification, and other important details.'+
+          'Our technology plays a crucial role in enabling traceability in the packing house. Every step of the sorting, cleaning, and packaging process is recorded and verified using our platform. This ensures that each operation adheres to our quality standards and compliance with regulations, and provides a verifiable record of the ginger journey from the packing house to the consumers table.',
+        gallery: [gingerp1.src, gingerp2.src],
       },
       {
-        title: 'Plantation',
+        title: 'Finished product - milestone 3',
         description:
-          'Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size. Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.',
-        gallery: [sampleImg.src],
+          'The finished product of organic fresh ginger is the culmination of our commitment to quality, sustainability, and traceability. Our platform ensures that the final processing steps, packaging, and labeling details are recorded and verified, allowing end consumers to trace the ginger back to its source in Pichanaki, Peru, and providing them with confidence in the product quality, sustainability, and origin.'+
+          'Once the ginger has undergone the sorting, cleaning, and packaging procedures at our packing house, it is ready to be delivered to consumers worldwide. Our packaging is designed to preserve the freshness and flavor of the ginger, ensuring that it arrives in the best possible condition to our customers.'+
+          'Our technology enables end consumers to verify the authenticity of the ginger through blockchain and food chain solutions. By scanning a QR code or accessing our online platform, consumers can access the complete traceability record of the ginger, including details about its cultivation, packing, and transportation. This empowers consumers to make informed choices, knowing that they are purchasing a premium product that is of the highest quality, sustainable, and authentic.',
+        gallery: [gingere1.src, gingere2.src],
       },
       {
-        title: 'Plantation',
+        title: 'Storage - milestone 4',
         description:
-          'Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size. Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.Our blueberries are delicately harvested and packed in the Cañete region of Peru, being fully bloomed and large in size.',
+          'Optimal storage conditions are crucial for preserving the freshness and flavor of organic fresh ginger, and our platform ensures traceability in the storage process.'+
+          'After the ginger is packaged, it is carefully stored in climate-controlled facilities that are designed to maintain the ginger freshness and flavor. Our platform records and verifies the storage conditions, ensuring that the ginger is stored at the optimal temperature and humidity levels to prevent deterioration and maintain its superior quality.'+
+          'Our commitment to traceability extends to every stage of the supply chain, including storage. By monitoring and recording the storage conditions, we can ensure that the ginger remains fresh and flavorful until it reaches the end consumers table. This meticulous attention to storage helps us deliver a premium product that meets our customers expectations and maintains the ginger natural goodness from Pichanaki, Peru, to consumers worldwide.',
         gallery: [sampleImg.src, sampleImg.src],
       },
     ],
