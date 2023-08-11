4e19cd76b7dceab3105469b078f479b7d9916465
pokemon918 <j27394645@gmail.com>
2023-08-11 11:59:47 -0400

    add icon inside the search box

--- packages/common/ui/SearchBox.tsx
+++ packages/common/ui/SearchBox.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, FC } from 'react'
 import { makeStyles } from '../utils'
+import searchIcon from '../assets/images/search-icon.png'
 
 interface InputProps {
     type?: 'text' | 'number' | 'email' | 'password'
@@ -15,7 +16,7 @@ interface InputProps {
 export const SearchBox: FC<InputProps> = (props) => {
     const styles = useStyles(props)
     return (
-        <>
+        <div css={styles.inputWrapper}>
             <label htmlFor={props.label}>{props.label}</label>
             <input
                 type={props.type}
@@ -27,17 +28,32 @@ export const SearchBox: FC<InputProps> = (props) => {
                 disabled={props.disabled}
                 css={styles.input}
             />
+            <img css={styles.icon} src={searchIcon.src} alt="searchIcon" />
             {props.error && <p className="error">Input filed can't be empty!</p>}
-        </>
+        </div>
     )
 }
 
 const useStyles = makeStyles(() => ({
-    input: {
+    inputWrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         border: '1px solid #3BA83B',
         borderRadius: 5,
+        background: '#fff',
+    },
+    input: {
+        border: 'none',
+        outline: 'none',
         fontSize: 18,
         fontWeight: 500,
         padding: '10px 20px',
+        background: 'transparent',
+    },
+    icon: {
+        width: 25,
+        height: 25,
+        marginRight: 15,
     }
 }))
