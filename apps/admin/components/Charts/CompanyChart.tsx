fa2e4b3197769669e86cbfe07e716f00c5ebdeae
pokemon918 <j27394645@gmail.com>
2023-09-11 10:46:56 +0900

    feat: dashboard init

---
+++ apps/admin/components/Charts/CompanyChart.tsx
@@ -0,0 +1,103 @@
+import { ApexOptions } from "apexcharts";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+// @ts-ignore
+const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+interface CompanyChartState {
+  series: number[];
+}
+
+const options: ApexOptions = {
+  chart: {
+    type: "donut",
+  },
+  colors: ["#10B981", "#375E83"],
+  labels: ["Buyer", "Supplier"],
+  legend: {
+    show: true,
+    position: "bottom",
+  },
+
+  plotOptions: {
+    pie: {
+      donut: {
+        size: "65%",
+        background: "transparent",
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  responsive: [
+    {
+      breakpoint: 2600,
+      options: {
+        chart: {
+          width: 380,
+        },
+      },
+    },
+    {
+      breakpoint: 640,
+      options: {
+        chart: {
+          width: 200,
+        },
+      },
+    },
+  ],
+};
+
+const CompanyChart: React.FC = () => {
+  const [state, setState] = useState<CompanyChartState>({
+    series: [80, 20],
+  });
+
+  return (
+    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-4">
+      <div className="mb-3 justify-between gap-4 sm:flex">
+        <div>
+          <h5 className="text-xl font-semibold text-black dark:text-white">
+            Companies
+          </h5>
+        </div>
+      </div>
+
+      <div className="mb-2">
+        <div id="companyChart" className="mx-auto flex justify-center">
+          <ReactApexChart
+// @ts-ignore
+            options={options}
+            series={state.series}
+            type="donut"
+          />
+        </div>
+      </div>
+
+      <div className="-mx-8 flex flex-wrap items-center justify-center gap-y-3">
+        <div className="w-full px-8 sm:w-1/2">
+          <div className="flex w-full items-center">
+            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-primary"></span>
+            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+              <span> Buyer </span>
+              <span> 80% </span>
+            </p>
+          </div>
+        </div>
+        <div className="w-full px-8 sm:w-1/2">
+          <div className="flex w-full items-center">
+            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#6577F3]"></span>
+            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+              <span> Supplier </span>
+              <span> 20% </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyChart;
---
+++ apps/admin/components/Charts/UserChart.tsx
@@ -0,0 +1,112 @@
+import { ApexOptions } from "apexcharts";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+// @ts-ignore
+const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+interface UserChartState {
+  series: number[];
+}
+
+const options: ApexOptions = {
+  chart: {
+    type: "donut",
+  },
+  colors: ["#375E83", "#259AE6", "#FFA70B"],
+  labels: ["Buyer", "Supplier", "Investor"],
+  legend: {
+    show: true,
+    position: "bottom",
+  },
+
+  plotOptions: {
+    pie: {
+      donut: {
+        size: "65%",
+        background: "transparent",
+      },
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  responsive: [
+    {
+      breakpoint: 2600,
+      options: {
+        chart: {
+          width: 380,
+        },
+      },
+    },
+    {
+      breakpoint: 640,
+      options: {
+        chart: {
+          width: 200,
+        },
+      },
+    },
+  ],
+};
+
+const UserChart: React.FC = () => {
+  const [state, setState] = useState<UserChartState>({
+    series: [30, 50, 20],
+  });
+
+  return (
+    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-4">
+      <div className="mb-3 justify-between gap-4 sm:flex">
+        <div>
+          <h5 className="text-xl font-semibold text-black dark:text-white">
+            Users
+          </h5>
+        </div>
+      </div>
+
+      <div className="mb-2">
+        <div id="userChart" className="mx-auto flex justify-center">
+          <ReactApexChart
+// @ts-ignore
+            options={options}
+            series={state.series}
+            type="donut"
+          />
+        </div>
+      </div>
+
+      <div className="-mx-8 flex flex-wrap items-center justify-center gap-y-3">
+        <div className="w-full px-8 sm:w-1/2">
+          <div className="flex w-full items-center">
+            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-primary"></span>
+            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+              <span> Buyer </span>
+              <span> 30% </span>
+            </p>
+          </div>
+        </div>
+        <div className="w-full px-8 sm:w-1/2">
+          <div className="flex w-full items-center">
+            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#6577F3]"></span>
+            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+              <span> Supplier </span>
+              <span> 50% </span>
+            </p>
+          </div>
+        </div>
+        <div className="w-full px-8 sm:w-1/2">
+          <div className="flex w-full items-center">
+            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#8FD0EF]"></span>
+            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+              <span> Investors </span>
+              <span> 20% </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserChart;
