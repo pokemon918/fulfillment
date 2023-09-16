7e4fb1fba1a4f576b6cc67d01e207298fd389cf1
pokemon918 <j27394645@gmail.com>
2023-09-16 11:18:03 +0900

    fix: update investmentForm.tsx

--- packages/common/components/investment-form/InvestmentForm.tsx
+++ packages/common/components/investment-form/InvestmentForm.tsx
@@ -554,6 +554,7 @@ export const InvestmentForm: FC<InvestmentFormProps> = ({
                     'delete'
                   )
                 }
+                log_name='investment'
                 redirect={`/investments`}
                 itemType="investment"
               />
